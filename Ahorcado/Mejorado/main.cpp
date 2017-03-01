213212bdefb085a49f12bef068ebd6e8f7bb9a96
Naomi M. Honti <naomiperla.macias@hotmail.com>
2017-03-01 08:33:37 -0600

    Update and rename Codigo.cpp to main.cpp

--- Ahorcado/Mejorado/main.cpp
+++ Ahorcado/Mejorado/main.cpp
@@ -1,7 +1,6 @@
 /**
  * Naomi Macias Honti a01282098
  * 28 de febrero 2017
- * 56 lineas mas 7 de comentarios. 63 en total.
  * 1 hr 30 min.
  * Agarra un listado de palabras con una longitud maxima de 15 de un archivo txt, muestra el escenario donde estan los intentos previos y lo que se ha encontrado de la palabra. Recibe un caracter del usuario y comprueba si ya fue usado, si esta en la palabra o si ya perdiste las 7 vidas.
  */
