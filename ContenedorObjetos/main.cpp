8ef3a2a62e9f9f2e64c99e39cfa8690c13d9139b
Naomi M. Honti <naomiperla.macias@hotmail.com>
2017-03-01 08:21:35 -0600

    Create main.cpp

---
+++ ContenedorObjetos/main.cpp
@@ -0,0 +1,57 @@
+/**
+ * Naomi Macias Honti a01282098
+ * 27 de febrero 2017
+ * 50 lineas mas 7 de comentarios. 57 en total.
+ * 3 hr.
+ * Lee los datos de un archivo csv y permite buscar en base a la informacion obtenida, borrar y crear nuevos datos en la lista que se guardan.
+ */
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#include "ContenedorAlumnos.h"
+
+
+int main()
+{
+    ifstream archivo;
+    archivo.open("Contenedor.csv");
+    ContenedorAlumnos documento;
+    documento.cargarDatos(archivo);
+    documento.enviarDatos();
+
+    string palabra, parametro, respuesta;
+    char letra;
+
+    cout << "Quiere buscar informacion? (si/no) ";
+    cin >> respuesta;
+    letra=tolower(respuesta[0]);
+    if (letra=='s'){
+        cout << "Buscar (palabra clave o numero): ";
+        cin >> palabra;
+        cout << "En (nombre/matricula/carrera): ";
+        cin >> parametro;
+        documento.buscarDatos(palabra,parametro);
+    }
+
+    cout << "Quiere agregar alumnos? (si/no) ";
+    cin >> respuesta;
+    letra=tolower(respuesta[0]);
+    if (letra=='s'){
+        documento.agregarDatos();
+    }
+    cout << "Quiere borrar alumnos? (si/no) ";
+    cin >> respuesta;
+    letra=tolower(respuesta[0]);
+    if (letra=='s'){
+        documento.eliminarDatos();
+    }
+    cout << "Quiere actualizar el archivo? (si/no) ";
+    cin >> respuesta;
+    letra=tolower(respuesta[0]);
+    if (letra=='s'){
+        documento.actualizarDatos();
+    }
+    return 0;
+}
