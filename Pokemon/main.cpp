e72d5ba33675cc65e2c5946c99f114e6cbd37e92
Naomi M. Honti <naomiperla.macias@hotmail.com>
2017-03-22 21:43:22 -0600

    Programa

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,7 +1,6 @@
 /**
  * Naomi Macias Honti a01282098
  * 17 de enero 2017
- * 59 lineas mas 7 de comentarios. 66 en total.
  * 27 min.
  * Recibe una velocidad y calcula el tiempo y la distancia con la que caera el objeto, si llega cerca a 3 metros atrapas el pokemon.
  */
