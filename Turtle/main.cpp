4517017d544cbfb44967f3b44e00fe58d006c944
Naomi M. Honti <naomiperla.macias@hotmail.com>
2017-04-21 17:20:45 -0500

    Programa

---
+++ Turtle/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Naomi Macias Honti a01282098
+ * 21 de abril 2017
+ * Tiempo no estimado.
+ * Lee un archivo con instrucciones para crear un dibujo en un arreglo que muestra y arregla .
+ */
+#include <iostream>
+
+using namespace std;
+
+#include "Turtle.h"
+
+int main() {
+   Turtle turtle("solidGun.txt");
+   int instr=0;
+
+   turtle.clearArea() ; //Clear the drawing area
+   while (instr != 9)
+      instr = turtle.move();
+
+   turtle.printArea();
+   return 0;
+}
