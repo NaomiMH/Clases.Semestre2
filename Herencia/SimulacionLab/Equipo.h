7297a9d834a7dd7aeba42ecc95c74e2f6b5b88e2
Naomi M. Honti <naomiperla.macias@hotmail.com>
2017-08-26 10:55:07 -0500

    Clase

---
+++ Herencia/SimulacionLab/Equipo.h
@@ -0,0 +1,18 @@
+#ifndef EQUIPO_H
+#define EQUIPO_H
+
+using namespace std;
+
+#include <iostream>
+
+class Equipo
+{
+    public:
+        Equipo(){}
+        virtual ~Equipo(){}
+        void esperar(int tiempo){}
+
+    private:
+};
+
+#endif // EQUIPO_H
